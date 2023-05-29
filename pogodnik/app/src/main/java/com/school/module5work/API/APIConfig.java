a2a06bbafede06caad4a024170d9fd4d1d074901
Rayni777 <116340605+Rayni777@users.noreply.github.com>
2023-05-29 13:39:49 +0300

    Add files via upload

---
+++ pogodnik/app/src/main/java/com/school/module5work/API/APIConfig.java
@@ -0,0 +1,9 @@
+package com.school.module5work.API;
+
+public class APIConfig {
+    public static final String HOST_URL = "https://api.weather.yandex.ru/";
+    public static final String LANG = "ru_RU";
+    public static final double CITY_EKB_LAT = 56.8519000;
+    public static final double CITY_EKB_LON = 60.6122000;
+    public static final String API_YANDEX_KEY = "c31bf7d9-58fb-4f58-bb4f-eceb4c4b427c";  //погода на вашем сайте
+}
